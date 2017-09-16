cf3db301c9fea2d889d802ae870e13009634d20c
siamumar <s2hussai@ieng6-244.ucsd.edu>
2017-09-16 11:38:13 -0700

    edit synopsys compile for TinyGarble

--- Netlist/compile.sh
+++ Netlist/compile.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 mkdir -p syn
 if [ $1 == "1" ]; then
-	design_vision -no_gui -f intersections_seq_syn.dcsh
+	design_vision -no_gui -f intersections_seq_TG_syn.dcsh
 fi 
 if [ $2 == "1" ]; then
-	design_vision -no_gui -f intersections_comb_syn.dcsh
+	design_vision -no_gui -f intersections_comb_TG_syn.dcsh
 fi 
 if [ $3 == "1" ]; then
-	design_vision -no_gui -f inside_syn.dcsh
+	design_vision -no_gui -f inside_TG_syn.dcsh
 fi
 rm *.pvl *.syn *.mr *.log *.svf
