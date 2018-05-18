c5a01f468c92c9f7835605db53204c74bb0a8ab4
siamumar <siamumar@ucsd.edu>
2018-05-18 14:11:57 -0700

    Update BMR code to new input format, add Vivado project tck file

--- Netlist/TriLoc_BMR.v
+++ Netlist/TriLoc_BMR.v
@@ -1,10 +1,11 @@
 `timescale 1ns / 1ps
 
 module TriLoc_BMR #(parameter N = 8)(
-	input	[2*N-1:0]	p0_input, //A
-	input	[2*N-1:0]	p1_input, //B
-	input	[2*N-1:0]	p2_input, //C
-	input	[3*N+2:0]	p3_input, //Q
+	input	[9*N+2:0]	p_input,	//A, B, C: 2*N; Q:3*(N+1) 
+	//input	[2*N-1:0]	p0_input, //A
+	//input	[2*N-1:0]	p1_input, //B
+	//input	[2*N-1:0]	p2_input, //C
+	//input	[3*N+2:0]	p3_input, //Q
 	output	[2*N+7:0]	o
     );
 	
@@ -13,15 +14,15 @@ module TriLoc_BMR #(parameter N = 8)(
 	
 	wire	signed	[N+3:0]	xM, yM;
 	
-	assign	xA	=	p0_input[2*N-1:N];
-	assign	yA	=	p0_input[N-1:0];
-	assign	xB	=	p1_input[2*N-1:N];
-	assign	yB	=	p1_input[N-1:0];
-	assign	xC	=	p2_input[2*N-1:N];
-	assign	yC	=	p2_input[N-1:0];
-	assign	rA	=	p3_input[3*N+2:2*N+2];
-	assign	rB	=	p3_input[2*N+1:N+1];
-	assign	rC	=	p3_input[N:0];
+	assign	xA	=	p_input[9*N+2:8*N+3];//p0_input[2*N-1:N];
+	assign	yA	=	p_input[8*N+2:7*N+3];//p0_input[N-1:0];
+	assign	xB	=	p_input[7*N+2:6*N+3];//p1_input[2*N-1:N];
+	assign	yB	=	p_input[6*N+2:5*N+3];//p1_input[N-1:0];
+	assign	xC	=	p_input[5*N+2:4*N+3];//p2_input[2*N-1:N];
+	assign	yC	=	p_input[4*N+2:3*N+3];//p2_input[N-1:0];
+	assign	rA	=	p_input[3*N+2:2*N+2];//p3_input[3*N+2:2*N+2];
+	assign	rB	=	p_input[2*N+1:N+1];//p3_input[2*N+1:N+1];
+	assign	rC	=	p_input[N:0];//p3_input[N:0];
 	
 	assign	o[2*N+7:N+4]	=	xM;
 	assign	o[N+3:0]		=	yM;
--- Netlist/TriLoc_BMR_tb.v
+++ Netlist/TriLoc_BMR_tb.v
@@ -2,23 +2,25 @@
 
 module TriLoc_BMR_tb;
 
-parameter N = 8;
+    parameter N = 8;
 
 	// Inputs	
-	wire	[2*N-1:0]	p0_input; //A
-	wire	[2*N-1:0]	p1_input; //B
-	wire	[2*N-1:0]	p2_input; //C
-	wire	[3*N+2:0]	p3_input; //Q
+	wire	[9*N+2:0]	p_input;	//A, B, C: 2*N; Q:3*(N+1) 
+	//wire	[2*N-1:0]	p0_input; //A
+	//wire	[2*N-1:0]	p1_input; //B
+	//wire	[2*N-1:0]	p2_input; //C
+	//wire	[3*N+2:0]	p3_input; //Q
 
 	// Outputs	
 	wire	[2*N+7:0]	o;
 	
 	// Instantiate the Unit Under Test (UUT)
 	TriLoc_BMR #(.N(N)) uut(
-		.p0_input(p0_input),
-		.p1_input(p1_input),
-		.p2_input(p2_input),
-		.p3_input(p3_input),
+		.p_input(p_input),
+		//.p0_input(p0_input),
+		//.p1_input(p1_input),
+		//.p2_input(p2_input),
+		//.p3_input(p3_input),
 		.o(o)
     );
 	
@@ -27,15 +29,15 @@ parameter N = 8;
 
 	wire	signed	[N+3:0]	xM, yM;	
 	
-	assign	p0_input[2*N-1:N]    	=	xA;
-	assign	p0_input[N-1:0]      	=	yA;
-	assign	p1_input[2*N-1:N]    	=	xB;
-	assign	p1_input[N-1:0]      	=	yB;
-	assign	p2_input[2*N-1:N]    	=	xC;
-	assign	p2_input[N-1:0]      	=	yC;
-	assign	p3_input[3*N+2:2*N+2]	=	rA;
-	assign	p3_input[2*N+1:N+1]  	=	rB;
-	assign	p3_input[N:0]        	=	rC;
+	assign	p_input[9*N+2:8*N+3]	=	xA;//p0_input[2*N-1:N]    	=	xA;
+	assign	p_input[8*N+2:7*N+3]	=	yA;//p0_input[N-1:0]      	=	yA;
+	assign	p_input[7*N+2:6*N+3]	=	xB;//p1_input[2*N-1:N]    	=	xB;
+	assign	p_input[6*N+2:5*N+3]	=	yB;//p1_input[N-1:0]      	=	yB;
+	assign	p_input[5*N+2:4*N+3]	=	xC;//p2_input[2*N-1:N]    	=	xC;
+	assign	p_input[4*N+2:3*N+3]	=	yC;//p2_input[N-1:0]      	=	yC;
+	assign	p_input[3*N+2:2*N+2]	=	rA;//p3_input[3*N+2:2*N+2]	=	rA;
+	assign	p_input[2*N+1:N+1]  	=	rB;//p3_input[2*N+1:N+1]  	=	rB;
+	assign	p_input[N:0]			=	rC;//p3_input[N:0]        	=	rC;
 	
 	assign	xM	=	o[2*N+7:N+4];
 	assign	yM	=	o[N+3:0];
@@ -56,6 +58,9 @@ parameter N = 8;
 		rA = 236;
 		rB = 183;
 		rC = 215;
+		
+		#100;
+		$display("%d\t%d", xM_3, yM_3);
 	end
 	  
 endmodule
